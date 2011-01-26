c4400319be33ea4aa7cd0b6c86c0f4216590b22c
yogione <smotheram@yahoo.com>
2011-01-25 20:07:51 -0500

    rem dep.rb

--- db/migrate/20110112020829_add_photo_path1_to_stories.rb
+++
@@ -1,9 +0,0 @@
-class AddPhotoPath1ToStories < ActiveRecord::Migration
-  def self.up
-    add_column :stories, :photo_path1, :string
-  end
-
-  def self.down
-    remove_column :stories, :photo_path1
-  end
-end
--- db/migrate/20110112020849_add_video_path1_to_stories.rb
+++
@@ -1,9 +0,0 @@
-class AddVideoPath1ToStories < ActiveRecord::Migration
-  def self.up
-    add_column :stories, :video_path1, :string
-  end
-
-  def self.down
-    remove_column :stories, :video_path1
-  end
-end
